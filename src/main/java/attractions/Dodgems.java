d0368d3560e71732438ddef8590a4bc867f3d197
Grant Houston <grant.houston44@hotmail.com>
2020-02-04 17:24:14 +0000

    ITicketed tested in dodgems

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,10 +1,28 @@
 package attractions;
 
 import behaviours.IReviewed;
+import behaviours.ITicketed;
+import people.Visitor;
 
-public class Dodgems extends Attraction implements IReviewed {
+public class Dodgems extends Attraction implements IReviewed, ITicketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
     }
+
+    public double defaultPrice(){
+        return 4.50;
+    }
+
+    public double priceFor(Visitor visitor){
+        double price = 0;
+        if (visitor.getAge() < 12 ){
+            price = defaultPrice() / 2;
+        }
+        else{
+            price = defaultPrice();
+        }
+        return price;
+    }
+
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitorAdult;
+    Visitor visitorKid;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitorAdult = new Visitor(30, 1.5, 40.0);
+        visitorKid = new Visitor(11, 1.2, 40.0);
+
     }
 
 
@@ -29,4 +35,15 @@ public class DodgemTest {
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
     }
+
+    @Test
+    public void isChargedNormal(){
+        assertEquals(4.50, dodgems.priceFor(visitorAdult), 0.1);
+    }
+
+    @Test
+    public void yungDiscount(){
+        assertEquals(2.25, dodgems.priceFor(visitorKid),0.1);
+    }
+
 }
