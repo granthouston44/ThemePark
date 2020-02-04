4a15127f7e50c1e5f98c3d093aaaa9223eab789e
Grant Houston <grant.houston44@hotmail.com>
2020-02-04 16:25:06 +0000

    ISecurity tested in playgrou

--- src/main/java/attractions/Playground.java
+++ src/main/java/attractions/Playground.java
@@ -1,8 +1,20 @@
 package attractions;
 
-public class Playground extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class Playground extends Attraction implements ISecurity {
 
     public Playground(String name, int rating) {
         super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        boolean allowed = true;
+        if (visitor.getAge() >= 15){
+            allowed = false;
+        }
+
+        return allowed;
     }
 }
--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitorKid;
+    Visitor visitorAdult;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitorAdult = new Visitor(30, 1.2, 40.0);
+        visitorKid = new Visitor(14, 1.2, 40.0);
     }
 
     @Test
@@ -26,5 +31,16 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void tooOld(){
+
+        assertEquals(false, playground.isAllowedTo(visitorAdult));
+    }
+
+    @Test
+    public void isAgeAppropriate(){
+        assertEquals(true, playground.isAllowedTo(visitorKid));
     }
 }
